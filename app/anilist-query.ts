4f54905b2b7c5089f1b285ff710e846060006119
OxygenJam <14032378+OxygenJam@users.noreply.github.com>
2023-05-17 01:34:46 +0800

    changed to a regular expression approach and removed stringify from preprocessing prior to sending to AniList

--- app/anilist-query.ts
+++ app/anilist-query.ts
@@ -1,8 +1,8 @@
 // Here we define our query as a multi-line string
 // Storing it in a separate .graphql/.gql file is also possible
 export const AniListQuery = `
-query ($search: String) { # Define which variables will be used in the query (id)
-  Media (search: $search, type: MANGA) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+query ($search: String) {
+  Media (search: $search, type: MANGA) {
     id
     title {
       romaji
--- app/bot.ts
+++ app/bot.ts
@@ -25,7 +25,7 @@ const arw = chalk.green('>> ');
 
 //Init upon bot loading
 bot.once(Events.ClientReady,()=>{
-    console.log('Logged in! マドカ先輩 is up and running');
+    console.log(chalk.green('Logged in! '), 'マドカ先輩 is up and running');
 });
 
 
@@ -33,59 +33,57 @@ bot.once(Events.MessageCreate, (message)=>{
 
     if(message.author.bot) return;
 
-    let usersmessage = message.content;
-    if(usersmessage.indexOf('?')!=-1){
+    const
+        commandReg =  /\?<([^<>]{1,})>/g,
+        usersmessage = message.content;
 
-        usersmessage = usersmessage.substr(1, usersmessage.length-1);
+    for(const match of usersmessage.matchAll(commandReg)){
+        if(!match[1]) continue;
 
-        if(usersmessage.indexOf('<')===0 && usersmessage.indexOf('>')===usersmessage.length-1){
-            
-            const 
-                replyto = message.author.username,
-                avatar = message.author.id + '/' +  message.author.avatar;
-            
-            usersmessage = usersmessage.substr(1, usersmessage.length-2);
+        const
+            query = match[1],
+            replyto = message.author.username,
+            avatar = message.author.id + '/' +  message.author.avatar;
+        
+        mangaQuerySearch(usersmessage, 3)
+        .then((metadata)=>{
 
-            mangaQuerySearch(usersmessage, 3)
-            .then((metadata)=>{
+            console.log(arw, 'Preparing to send...');
 
-                console.log(arw, 'Preparing to send...');
-
-                const mangaResult = new EmbedBuilder({
-                    author:{
-                        name: metadata.title.romaji
-                    },
-                    fields:[
-                        { name: "Romaji", value: metadata.title.romaji },
-                        { name: "English", value: metadata.title.english },
-                        { name: "Native", value: metadata.title.native },
-                        { name: "Status", value: metadata.status },
-                    ],
-                    color: 11962048,
-                    timestamp: new Date(),
-                    footer:{
-                        icon_url: `https://cdn.discordapp.com/avatars/${avatar}.png`,
-                        text: `Requested by: ${replyto}`
-                    }
-                })
-
-                if(metadata.coverImage){
-                    mangaResult.setImage(metadata.coverImage.medium);
-                    console.log(arw, "Image properly loaded");
-                }else{
-                    console.log(err, "Image failed to load or unavailable");
+            const mangaResult = new EmbedBuilder({
+                author:{
+                    name: metadata.title.romaji
+                },
+                fields:[
+                    { name: "Romaji", value: metadata.title.romaji },
+                    { name: "English", value: metadata.title.english },
+                    { name: "Native", value: metadata.title.native },
+                    { name: "Status", value: metadata.status },
+                ],
+                color: 11962048,
+                timestamp: new Date(),
+                footer:{
+                    icon_url: `https://cdn.discordapp.com/avatars/${avatar}.png`,
+                    text: `Requested by: ${replyto}`
                 }
-
-                //console.log(arw, "Sending: \n", chalk.green(JSON.stringify(result)));
-                message.channel.send({embeds: [mangaResult]});
-                console.log(arw, 'Sent!');
-
-            }).catch((error)=>{
-                console.log(err, error);
-
-                message.reply(`すみません！ ${error.message}`);
             })
-        }
+
+            if(metadata.coverImage){
+                mangaResult.setImage(metadata.coverImage.medium);
+                console.log(arw, "Image properly loaded");
+            }else{
+                console.log(err, "Image failed to load or unavailable");
+            }
+
+            //console.log(arw, "Sending: \n", chalk.green(JSON.stringify(result)));
+            message.reply({embeds: [mangaResult]});
+            console.log(arw, 'Sent!');
+
+        }).catch((error)=>{
+            console.error(err, error);
+
+            message.reply(`すみません！ ${error.message}`);
+        })
     }
 });
 
@@ -114,10 +112,10 @@ function mangaQuerySearch(manga:string, retries:number): Promise<MangaResult>{
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
-        body: JSON.stringify({
+        body: {
             query: AniListQuery,
             variables: aniListArgs
-        })
+        }
     };
 
     return axios(query)
@@ -127,16 +125,16 @@ function mangaQuerySearch(manga:string, retries:number): Promise<MangaResult>{
         return result;
     })
     .catch((error) => {
-        if(error.response.status === 404){
-            throw 'No results found';
+        if(error.response.status === 400){
+            throw new Error('No results found');
         }
         else{
             if(retries>0){
-                console.log('ERROR: ', err);
+                console.error(err, error);
                 return mangaQuerySearch(manga, retries-1);
             }
             else{
-                throw 'Something went wrong while retrieving the manga';
+                throw new Error('Something went wrong while retrieving the manga');
             }
         }
     })
